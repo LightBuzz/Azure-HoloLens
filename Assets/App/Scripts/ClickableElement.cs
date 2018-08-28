21b66a7daeead0b3b4ba48238b3a703b50ab2581
Vangos Pterneas <vangos@lightbuzz.com>
2018-08-28 17:11:47 +0300

    Added a demo scene.

---
+++ Assets/App/Scripts/ClickableElement.cs
@@ -0,0 +1,16 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickableElement : MonoBehaviour, IInputClickHandler, IPointerClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Clicked!");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked!");
+    }
+}
