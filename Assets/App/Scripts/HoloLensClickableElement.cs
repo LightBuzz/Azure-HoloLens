5fcea40bbf3b61f840f9a522fa7e588052c6df8e
Vangos Pterneas <vangos@lightbuzz.com>
2018-08-29 18:10:31 +0300

    Working Editor demo.

---
+++ Assets/App/Scripts/HoloLensClickableElement.cs
@@ -0,0 +1,48 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class HoloLensClickableElement : MonoBehaviour, IInputClickHandler, IPointerClickHandler, IFocusable
+{
+    [SerializeField] private TextMesh text;
+
+    public UnityEvent OnClick;
+
+    public TodoItem Item { get; set; }
+
+    public void Setup(TodoItem item)
+    {
+        Item = item;
+
+        if (text == null)
+        {
+            text = GetComponentInChildren<TextMesh>();
+        }
+        text.text = item.Text;
+    }
+
+    public void OnFocusEnter()
+    {
+        Debug.Log("Focus enter");
+        transform.localScale *= 1.2f;
+    }
+
+    public void OnFocusExit()
+    {
+        Debug.Log("Focus exit");
+        transform.localScale /= 1.2f;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Clicked");
+        OnClick?.Invoke();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+        OnClick?.Invoke();
+    }
+}
---
+++ Assets/App/Scripts/LightBuzzAzureManager.cs
@@ -0,0 +1,50 @@
+﻿using LightBuzz.Azure;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LightBuzzAzureManager : MonoBehaviour
+{
+    private LightBuzzMobileServiceClient azureClient;
+    private AppServiceTableDAO<TodoItem> todoTableDAO;
+    private List<TodoItem> todoItems;
+
+    [Tooltip("User interface")]
+    [SerializeField]
+    private HoloLensClickableElement element;
+
+    [SerializeField]
+    [Tooltip("The Azure App Service URL")]
+    private string mobileAppUri = "https://testtodolightbuzz.azurewebsites.net";
+
+    [SerializeField]
+    [Tooltip("Support local database")]
+    private bool supportLocalDatabase = true;
+
+    private async void Start()
+    {
+        await Init();
+    }
+
+    private async Task Init()
+    {
+        // Initialize Azure
+        azureClient = new SampleMobileClient(mobileAppUri, supportLocalDatabase);
+        await azureClient.InitializeLocalStore();
+
+        // Retrieve the items from the server
+        todoTableDAO = new AppServiceTableDAO<TodoItem>(azureClient);
+        todoItems = await todoTableDAO.FindAll();
+
+        // Populate the UI
+        float xOffset = 0f;
+
+        foreach (TodoItem item in todoItems)
+        {
+            HoloLensClickableElement obj = Instantiate(element, new Vector3(xOffset, 0f, 8f), new Quaternion());
+            obj.Setup(item);
+
+            xOffset += obj.transform.localScale.x + 0.1f;
+        }
+    }
+}
